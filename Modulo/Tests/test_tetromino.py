f1636e795b934883bc35fbf5225d6a0f7e42ba84
emanuelongo <emanuel1001cano@gmail.com>
2023-11-02 09:51:12 -0500

    test

--- Modulo/Tests/test_tetromino.py
+++ Modulo/Tests/test_tetromino.py
@@ -1,1 +1,12 @@
+from Modulo.Modelo.Tetrominos import Tridominos
+
+
 def test_tetromino_gira_90_grados():
+    # Arrange
+
+    tetromino = Tridominos()
+
+    # Act
+    resultado = tetromino.rotar_90_grados_i()
+
+    # Assert
